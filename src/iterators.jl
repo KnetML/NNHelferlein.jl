068d78ea9f38b6b17996f2c3448db2076fba843b
Andreas Dominik <andreas.dominik@mni.thm.de>
2023-07-18 11:03:29 +0200

    mask types fixed

--- src/iterators.jl
+++ src/iterators.jl
@@ -265,7 +265,10 @@ Base.length(it::MBMasquerade) = length(it.it)
 function do_mask(x, ρ, value)
     
     mask = rand(size(x)...) .< ρ
-    x = x .- value
+    mask = ones(eltype(x), size(x)) .* mask       # make bool to numeric
+    mask = typeof(x)(mask)                         # make syme type as x
+    value = eltype(x)(value)
+    x = x .- value                                  # set msaked positions to value
     x = x .* mask
     x = x .+ value
     return x
